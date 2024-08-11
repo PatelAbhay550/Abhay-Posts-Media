2d1e3c06d42248707c6f5b77cce4bd55b1a3b7ed
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-08-11 21:58:19 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Abhay Posts Media | Share Your with the World",
+  title: "Abhay Posts Media | Share Your Thoughts with the World",
   description: "Share Your Thoughts and Media with the World and get feedback",
 };
 
